dcb0e8a7b263283cd693569f0893ab31393cd588
angelnjuarez <angelnjuarez@gmail.com>
2024-08-26 11:52:56 -0300

    28-8 recursion

--- src/clase5Recursion/Test.java
+++ src/clase5Recursion/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println(RecursionParcial.espejar("casa"));
+		System.out.println(RecursionParcial.estaPrimera("", "perro"));
+
 	}
 
 }
