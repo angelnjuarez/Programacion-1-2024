5cab7d815a7afe6e34c9b1abf3b9a3362526d3c4
angelnjuarez <angelnjuarez@gmail.com>
2024-09-11 11:10:07 -0300

    Excepciones final

---
+++ src/clase10Exception/EjeException.java
@@ -0,0 +1,66 @@
+package clase10Exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class EjeException {
+
+	static char noVacioEntoncesA(String s) {
+		if (s.length() == 0) {
+			// System.out.println("El String está vacio");
+			throw new RuntimeException("El String está vacio");
+		}
+		return 'a';
+	}
+
+	public static void main(String[] args) {
+
+		/*
+		 * Excepciones de Java: ArithmeticException
+		 */
+		// int i = 10/0;
+
+		/*
+		 * Error al parsear
+		 */
+		// String s = "5d";
+		// int x = Integer.parseInt(s);
+
+		/*
+		 * Bloque try/catch
+		 */
+//		String abc = "abcdefghijklmnopqrstuvwxyz";
+//		boolean ok = false;
+//		while (!ok) {
+//			try {
+//				Scanner scan = new Scanner(System.in);
+//				System.out.println("Ingrese el numero de letra del abecedario " + "que quiere consultar:");
+//				int i = scan.nextInt();
+//
+//				System.out.println("La letra numero " + i + " es " + abc.charAt(i - 1));
+//				ok = true;
+//			} catch (StringIndexOutOfBoundsException ex) {
+//				System.out.println("El numero debe estar entre 1 y " + abc.length());
+//			} catch (InputMismatchException ex) {
+//				System.out.println("El valor ingresado no es un numero");
+//			} catch (Exception ex) {
+//				System.out.println("Tuve otro problema: " + ex.toString());
+//			}
+//		}
+
+		/*
+		 * Excepciones propias
+		 */
+
+		try {
+			String s = "Hola";
+			System.out.println(noVacioEntoncesA(s));
+		} catch (Exception ex) {
+			System.out.println("Se produjo el error: " + ex.getMessage());
+		} finally {
+			System.out.println("Siempre se ejecuta esto, haya o no error");
+		}
+		System.out.println("Llegué acá");
+
+	}
+}
