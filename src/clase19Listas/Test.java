c88382bcb8c86d1ce1b9c532d454cac8907d29f1
angelnjuarez <angelnjuarez@gmail.com>
2024-10-16 10:08:36 -0300

    listas

---
+++ src/clase19Listas/Test.java
@@ -0,0 +1,22 @@
+package clase19Listas;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 ListaInt lis=new ListaInt();
+		 lis.agregarAdelante(0);
+		 lis.agregarAdelante(2);
+		 lis.agregarAdelante(-18);
+		 lis.agregarAdelante(122);
+		 lis.agregarAdelante(15);
+		 lis.agregarAtras(122);
+		 lis.agregarAtras(32);
+		 lis.imprimir();
+		 lis.quitar(32);
+		 lis.imprimir();
+		 System.out.println(lis.largo());
+		//System.out.println(lis.toString());
+
+	}
+
+}
